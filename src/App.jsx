bbed16bb7f99ee9e7c49e8a1ff5582f327d1b20a
Ron Bachar <ron.bachar@outlook.com>
2024-02-21 10:06:52 +0200

    removed last changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,9 @@ import { useState } from "react";
 import Sidenav from "./components/Sidenav";
 import Main from "./components/Main";
 import Work from "./components/Work";
-import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import Technology from "./components/Technology";
-import Resume from "./components/Resume";
 
 function App() {
   const [isNavOpen, setIsNavOpen] = useState(false); // Use useState here
