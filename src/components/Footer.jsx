9e35c28c3764b33396f2cd7c08694ee7bc4c6f16
Ron Bachar <ron.bachar@outlook.com>
2024-03-04 09:53:29 +0200

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
 import { VscGithub } from "react-icons/vsc";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
 
-function Footer() {
+const Footer = () => {
   return (
     <div className="flex justify-center gap-6 py-6">
-      <a href="https://github.com/RonBachar" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://github.com/RonBachar"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <VscGithub className="text-2xl text-[#001b5e]" />
       </a>
-      <a href="https://www.linkedin.com/in/ron-bachar/" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://www.linkedin.com/in/ron-bachar/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <FaLinkedinIn className="text-2xl text-[#001b5e]" />
       </a>
       <a href="tel:0544631809">
@@ -17,6 +25,6 @@ function Footer() {
       </a>
     </div>
   );
-}
+};
 
 export default Footer;
